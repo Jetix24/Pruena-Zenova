5a5cb41f9165deb25ce70f898d9bf291f415f793
Jetix24 <josepabloaguirre02@gmail.com>
2025-03-26 01:45:48 -0600

    Update: ProductList

--- src/component/ProductsList.js
+++ src/component/ProductsList.js
@@ -1,7 +1,13 @@
 export default function ProductsList() {
   return (
     <div>
-      <h1>Lista de productos</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1>Lista de productos</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
