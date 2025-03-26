c34fdc052675c63dfff16057857228953d85087c
Jetix24 <josepabloaguirre02@gmail.com>
2025-03-26 01:46:01 -0600

    Update: CategoriesList

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./component/Header";
 import Home from "./component/Home";
 import ProductsList from "./component/ProductsList";
+import CategoriesList from "./component/CategoriesList";
+import Footer from "./component/Footer";
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<ProductsList />} />
+        <Route path="/categories" element={<CategoriesList />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/component/CategoriesList.js
@@ -0,0 +1,7 @@
+export default function CategoriesList() {
+  return (
+    <div>
+      <h1>Lista de categorias</h1>
+    </div>
+  );
+}
